3f7db5d892141690710ae309786e213d581a71e6
DurkaMateusz <mosca888@gmail.com>
2023-09-09 00:47:51 +0200

    feature-modal-our_products add

--- src/partials/modal.js
+++ src/partials/modal.js
@@ -15,6 +15,21 @@
 
 (() => {
   const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-2]'),
+    closeModalBtn: document.querySelector('[data-modal-close-2]'),
+    modal: document.querySelector('[data-modal-2]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
+(() => {
+  const refs = {
     openModalBtn: document.querySelector('[data-modal-open-3]'),
     closeModalBtn: document.querySelector('[data-modal-close-3]'),
     modal: document.querySelector('[data-modal-3]'),
