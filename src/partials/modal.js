873761aa4e394d3bcfae74cb8b0a37aca976aee5
DurkaMateusz <mosca888@gmail.com>
2023-09-09 00:03:56 +0200

    modal-franchise add

--- src/partials/modal.js
+++ src/partials/modal.js
@@ -12,3 +12,18 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-5]'),
+    closeModalBtn: document.querySelector('[data-modal-close-5]'),
+    modal: document.querySelector('[data-modal-5]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
